5101a74723f351562858b690871e00346990b2fc
Rob Brennan <rob@therobbrennan.com>
2018-05-14 18:41:35 -0700

    Added link to ping in the app layout

--- layout/App.jsx
+++ layout/App.jsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 export default ({ children }) => {
   return (
     <div>
-      <Link href={"/"}>
-        <a>Home</a>
-      </Link>
+      <Link href={"/"}><a>Home</a></Link>&nbsp;
+      <Link href={"/ping"}><a>Ping</a></Link>&nbsp;
+
       {children}
     </div>
   )
