361573619f4b848e0315ea6eda22031267f2bb70
Rob Brennan <rob@therobbrennan.com>
2018-05-14 18:31:58 -0700

    Index page is now rendering content using the new layout component and inline styling

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,25 @@
+import App from '../layout/App'
 import { dependencies } from '../package.json'
 const { next: nextJSVersion, react: reactVersion } = dependencies
 
-export default () => <div>[DEMO] NextJS {nextJSVersion} and React {reactVersion}</div>
+export default () => (
+  <App>
+    <div className="heading">
+      [DEMO] NextJS {nextJSVersion} and React {reactVersion}
+    </div>
+    <style>{`
+      /* Sample inline styles */
+      a, a:visited, a:active {
+        color: inherit
+      }
+      a:hover {
+        color: #8B008B
+      }
+
+      .heading {
+        font-size: 50px
+      }
+    `}
+    </style>
+  </App>
+)
