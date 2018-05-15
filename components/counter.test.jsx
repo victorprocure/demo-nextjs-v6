279e58a86535861d2b07bb6b5991d9b37054c3a2
Rob Brennan <rob@therobbrennan.com>
2018-05-15 09:00:22 -0700

    Counter test now verifies the connected Redux component renders as well as verify the add method is invoked

--- components/counter.test.jsx
+++ components/counter.test.jsx
@@ -1,8 +1,9 @@
 import { shallow } from "enzyme"
 import { Provider } from 'react-redux'
-import Counter from './counter'
+import ConnectedComponent, { Counter } from './counter'
+import { wrap } from "module";
 
-describe("The Counter component", () => {
+describe("The connected Redux component", () => {
   it("should render", () => {
     const mockStore = {
       subscribe: jest.fn(),
@@ -14,9 +15,26 @@ describe("The Counter component", () => {
     }
     const wrapper = shallow(
       <Provider store={mockStore} >
-        <Counter {...mockProps} />
+        <ConnectedComponent {...mockProps} />
       </Provider>
     )
     expect(wrapper).toBeDefined()
+  })
+})
+describe(`Counter`, () => {
+  it(`should be defined`, () => {
+    expect(Counter).toBeDefined()
+  })
+  describe(`and when the add button is pressed`, () => {
+    let wrapper
+    const mockAdd = jest.fn()
+    beforeEach(() => {
+      wrapper = shallow(<Counter add={mockAdd} />)
+      // Click the add button
+      wrapper.find('button#add').simulate('click')
+    })
+    it(`the add function should be invoked`, () => {
+      expect(mockAdd.mock.calls.length).toBe(1)
+    })
   })
 })
