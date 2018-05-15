7a8ab41dbdb9fa92099648a49251cb4f4d1afa35
Rob Brennan <rob@therobbrennan.com>
2018-05-14 18:42:51 -0700

    Added front-end routing example links

--- layout/App.jsx
+++ layout/App.jsx
@@ -5,6 +5,9 @@ export default ({ children }) => {
     <div>
       <Link href={"/"}><a>Home</a></Link>&nbsp;
       <Link href={"/ping"}><a>Ping</a></Link>&nbsp;
+      <Link as={`/people`} href={`/ping`}><a>People</a></Link>&nbsp;
+      <Link as={`/people/developers`} href={`/ping?slug=developers`}><a>Developers</a></Link>&nbsp;
+      <Link as={`/people/developers/rob`} href={`/ping?slug=developers&name=rob`}><a>Rob</a></Link>&nbsp;
 
       {children}
     </div>
