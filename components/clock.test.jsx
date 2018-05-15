b0c77d58eea125a8c0ed460893c6b50a51762ff7
Rob Brennan <rob@therobbrennan.com>
2018-05-14 22:22:33 -0700

    Added test for the clock component

---
+++ components/clock.test.jsx
@@ -0,0 +1,21 @@
+import { mount } from "enzyme"
+import Clock from "./clock"
+
+describe("The Clock component", () => {
+  it("should render", () => {
+    const wrapper = mount(<Clock />)
+    expect(wrapper).toBeDefined()
+  })
+  describe(`and when supplied with`, () => {
+    describe(`a lastUpdate prop`, () => {
+      describe(`that is a number`, () => {
+        const mockLastUpdateProp = new Date()
+        it("should render", () => {
+          const wrapper = mount(<Clock lastUpdate={mockLastUpdateProp} />)
+          console.log(wrapper.debug())
+          expect(wrapper).toBeDefined()
+        })              
+      })
+    })
+  })
+})
