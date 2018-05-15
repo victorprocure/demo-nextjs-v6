a664564de354f1e1f325e8b340b0a519187afdf2
Rob Brennan <rob@therobbrennan.com>
2018-05-14 22:01:22 -0700

    Added index page using components interacting with the redux store

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {startClock, addCount, serverRenderClock} from '../store/store'
+import Examples from '../components/examples'
+
+class Counter extends React.Component {
+  static getInitialProps ({ reduxStore, req }) {
+    const isServer = !!req
+    reduxStore.dispatch(serverRenderClock(isServer))
+    reduxStore.dispatch(addCount())
+
+    return {}
+  }
+
+  componentDidMount () {
+    const {dispatch} = this.props
+    this.timer = startClock(dispatch)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
+  render () {
+    return (
+      <Examples />
+    )
+  }
+}
+
+export default connect()(Counter)
