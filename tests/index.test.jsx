b7b79274f97abb15770790619debb9bfa7df6820
Rob Brennan <rob@therobbrennan.com>
2018-05-15 18:33:00 -0700

    Deleted placeholder tests brought over during the rebase on the latest master

--- tests/index.test.jsx
+++
@@ -1,9 +0,0 @@
-import { mount } from "enzyme"
-import Index from "../pages/index"
-
-describe("The default page", () => {
-  it("should render", () => {
-    const wrapper = mount(<Index />)
-    expect(wrapper).toBeDefined()
-  })
-})
