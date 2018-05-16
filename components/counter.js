ed17015f781d5d502163c1c76430da1d1b56af8c
Rob Brennan <rob@therobbrennan.com>
2018-05-15 19:15:32 -0700

    Added inline styling to the counter component

--- components/counter.js
+++ components/counter.js
@@ -6,19 +6,35 @@ export class Counter extends Component {
   render() {
     const { count } = this.props
     return (
-      <div>
+      <div className="main">
         <h1>
           AddCount: <span>{count}</span>
         </h1>
-        <button id="increment" onClick={this.props.increment}>
-          +
-        </button>
-        <button id="reset" onClick={this.props.reset}>
-          Reset
-        </button>
-        <button id="decrement" onClick={this.props.decrement}>
-          -
-        </button>
+        <div>
+          <button id="increment" onClick={this.props.increment}>
+            <span>+</span>
+          </button>
+          <button id="reset" onClick={this.props.reset}>
+            <span>Reset</span>
+          </button>
+          <button id="decrement" onClick={this.props.decrement}>
+            <span>-</span>
+          </button>
+        </div>
+        <style>{`
+        .main {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: center;
+        }
+        button > span, p {
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          padding: 5px;
+        }        
+      `}</style>
       </div>
     )
   }
