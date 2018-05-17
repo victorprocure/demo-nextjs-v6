db97aa6628cf5d5bab0817eaf0ac91e1ad00cc2f
Rob Brennan <rob@therobbrennan.com>
2018-05-16 17:51:30 -0700

    Converted App layout component to TypeScript

--- layout/App.tsx
+++ layout/App.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link"
+import Link from 'next/link';
 
 export default ({ children }) => {
   return (
     <div>
-      <Link href={"/"}><a>Home</a></Link>&nbsp;
-      <Link href={"/ping"}><a>Ping</a></Link>&nbsp;
+      <Link href={'/'}><a>Home</a></Link>&nbsp;
+      <Link href={'/ping'}><a>Ping</a></Link>&nbsp;
       <Link as={`/people`} href={`/ping`}><a>People</a></Link>&nbsp;
       {/* EXAMPLE CODE: This is how you translate client side URLs into NextJS pages with parameters
         <Link as={`/people/developers`} href={`/ping?slug=developers`}><a>Developers</a></Link>&nbsp;
@@ -23,5 +23,5 @@ export default ({ children }) => {
     `}
     </style>
     </div>
-  )
-}
+  );
+};
