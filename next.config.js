b3aac30ff02a3b341ee90e74948dc504d142ad63
Rob Brennan <rob@therobbrennan.com>
2018-05-16 17:18:11 -0700

    Updated next.config.js to incorporate TypeScript

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
 const withSourceMaps = require('@zeit/next-source-maps')
+const withTypeScript = require('@zeit/next-typescript')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const { ANALYZE } = process.env
 
-module.exports = withSourceMaps({
-  webpack: function (config) {
-    if (ANALYZE) {
-      config.plugins.push(new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        analyzerPort: 8888,
-        openAnalyzer: true
-      }))
+module.exports = withTypeScript(
+  withSourceMaps({
+    webpack: function (config) {
+      if (ANALYZE) {
+        config.plugins.push(new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+          analyzerPort: 8888,
+          openAnalyzer: true
+        }))
+      }
+  
+      return config
     }
-
-    return config
-  }
-})
+  })  
+)
