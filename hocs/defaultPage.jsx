82a648ce6e67cc765ed92c97d353e86232ed1e98
Rob Brennan <rob@therobbrennan.com>
2018-05-19 21:52:20 -0700

    defaultPage HOC is now responsible for rendering the Header

--- hocs/defaultPage.jsx
+++ hocs/defaultPage.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Router from 'next/router'
 
+import Header from '../components/Header'
 import { getUserFromServerCookie, getUserFromLocalCookie } from '../utils/auth'
 
 // This higher order component (HoC) simply takes in a Page component and returns the enhanced component
@@ -39,6 +40,7 @@ export default Page =>
             <meta name='viewport' content='width=device-width, initial-scale=1' />
             <title>{pageTitle}</title>
           </Head>
+          <Header {...this.props} />
           <Page {...this.props} />
         </div>
       )
