cb4cfa4d653061e0fbc0f483036c7f9e16bdca6c
Rob Brennan <rob@therobbrennan.com>
2018-05-19 19:35:44 -0700

    Added styling of <a> tags to Header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -9,6 +9,16 @@ export default () => {
       <Link as={`/people/developers`} href={`/ping?slug=developers`}><a>Developers</a></Link>&nbsp;
       <Link as={`/people/developers/rob`} href={`/ping?slug=developers&name=rob`}><a>Rob</a></Link>&nbsp;
       <Link href={"/secret"}><a>Secret</a></Link>&nbsp;  
+      <style>{`
+      /* Sample inline styles */
+      a, a:visited, a:active {
+        color: inherit
+      }
+      a:hover {
+        color: #8B008B
+      }
+    `}
+    </style>
     </div>
   )
 }
