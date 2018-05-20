88b9808b9156b0b1de26ece19d7548c131940f9c
Rob Brennan <rob@therobbrennan.com>
2018-05-19 19:23:06 -0700

    Added auth/signed-in page

---
+++ pages/auth/signed-in.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'
+
+import Router from 'next/router'
+
+import { setToken, checkSecret, extractInfoFromHash } from '../../utils/auth'
+
+export default class SignedIn extends React.Component {
+  static propTypes = {
+    url: PropTypes.object.isRequired
+  }
+
+  componentDidMount () {
+    const { token, secret } = extractInfoFromHash()
+    if (!checkSecret(secret) || !token) {
+      console.error('Unexpected behavior with the Sign In request')
+    }
+    setToken(token)
+    Router.push('/')
+  }
+
+  render () {
+    return null
+  }
+}
