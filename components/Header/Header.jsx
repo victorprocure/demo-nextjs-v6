1c98ef95e415a7ceb425cda3fe0b569213594aa7
Rob Brennan <rob@therobbrennan.com>
2018-05-19 20:26:01 -0700

    Added significant functionality to the Header component to display links based on whether the user is authenticated or not

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,24 +1,49 @@
+import PropTypes from 'prop-types'
+
 import Link from "next/link"
 
-export default () => {
+const links = [
+  { href: '/', text: 'Home' },
+  { href: '/ping', text: 'Ping' },
+  { href: '/secret', text: 'Top Secret', authRequired: true },
+  { href: '/auth/sign-in', text: 'Sign In', anonymousOnly: true },
+  { href: '/auth/sign-off', text: 'Sign Off', authRequired: true }
+]
+
+const getAllowedLinks = isAuthenticated => links
+  .filter(l => !l.authRequired || (l.authRequired && isAuthenticated))
+  .filter(l => !isAuthenticated || (isAuthenticated && !l.anonymousOnly))
+
+const Header = ({ isAuthenticated, currentUrl }) => {
   return (
-    <div>
-      <Link href={"/"}><a>Home</a></Link>&nbsp;
-      <Link href={"/ping"}><a>Ping</a></Link>&nbsp;
-      <Link as={`/people`} href={`/ping`}><a>People</a></Link>&nbsp;
-      <Link as={`/people/developers`} href={`/ping?slug=developers`}><a>Developers</a></Link>&nbsp;
-      <Link as={`/people/developers/rob`} href={`/ping?slug=developers&name=rob`}><a>Rob</a></Link>&nbsp;
-      <Link href={"/secret"}><a>Secret</a></Link>&nbsp;  
+    <div id='header'>
+      {getAllowedLinks(isAuthenticated).map(l => (
+        <Link prefetch key={l.href} href={l.href}>
+          {l.text}
+        </Link>
+      ))}
       <style>{`
-      /* Sample inline styles */
-      a, a:visited, a:active {
-        color: inherit
-      }
-      a:hover {
-        color: #8B008B
-      }
-    `}
-    </style>
+        /* Sample inline styles */
+        a, a:visited, a:active {
+          color: inherit;
+        }
+        a:hover {
+          color: #8B008B
+        }
+      `}
+      </style>
     </div>
   )
 }
+
+Header.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+  currentUrl: PropTypes.string.isRequired
+}
+
+Header.defaultProps = {
+  isAuthenticated: false,
+  currentUrl: '/'
+}
+
+export default Header
