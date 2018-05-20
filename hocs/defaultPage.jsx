9448abe40d7ab4c709b7e275a05ba1ec4b94a71e
Rob Brennan <rob@therobbrennan.com>
2018-05-19 18:26:46 -0700

    Flushed out defaultPage HOC

--- hocs/defaultPage.jsx
+++ hocs/defaultPage.jsx
@@ -1,7 +1,46 @@
+import Head from 'next/head'
+import Router from 'next/router'
+
+import { getUserFromServerCookie, getUserFromLocalCookie } from '../utils/auth'
+
 // This higher order component (HoC) simply takes in a Page component and returns the enhanced component
 export default Page =>
   class DefaultPage extends React.Component {
+    static getInitialProps (ctx) {
+      const loggedUser = process.browser ? getUserFromLocalCookie() : getUserFromServerCookie(ctx.req)
+      const pageProps = Page.getInitialProps && Page.getInitialProps(ctx)
+      return {
+        ...pageProps,
+        loggedUser,
+        currentUrl: ctx.pathname,
+        isAuthenticated: !!loggedUser
+      }
+    }
+  
+    logout = (eve) => {
+      if (eve.key === 'logout') {
+        Router.push(`/?logout=${eve.newValue}`)
+      }
+    }
+  
+    componentDidMount () {
+      window.addEventListener('storage', this.logout, false)
+    }
+  
+    componentWillUnmount () {
+      window.removeEventListener('storage', this.logout, false)
+    }
+      
     render() {
-      return <Page {...this.props} />
+      const pageTitle = 'NextJS and Auth0'
+      return (
+        <div>
+          <Head>
+            <meta name='viewport' content='width=device-width, initial-scale=1' />
+            <title>{pageTitle}</title>
+          </Head>
+          <Page {...this.props} />
+        </div>
+      )
     }
   }
