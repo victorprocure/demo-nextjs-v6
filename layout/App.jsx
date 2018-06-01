3f9ffc19325740386b48da5510af8c8864ba51ae
Rob Brennan <rob@therobbrennan.com>
2018-05-31 20:18:21 -0700

    Identified example code for handling clean URLs on the front-end

--- layout/App.jsx
+++ layout/App.jsx
@@ -6,8 +6,10 @@ export default ({ children }) => {
       <Link href={"/"}><a>Home</a></Link>&nbsp;
       <Link href={"/ping"}><a>Ping</a></Link>&nbsp;
       <Link as={`/people`} href={`/ping`}><a>People</a></Link>&nbsp;
-      <Link as={`/people/developers`} href={`/ping?slug=developers`}><a>Developers</a></Link>&nbsp;
-      <Link as={`/people/developers/rob`} href={`/ping?slug=developers&name=rob`}><a>Rob</a></Link>&nbsp;
+      {/* EXAMPLE CODE: This is how you translate client side URLs into NextJS pages with parameters
+        <Link as={`/people/developers`} href={`/ping?slug=developers`}><a>Developers</a></Link>&nbsp;
+        <Link as={`/people/developers/rob`} href={`/ping?slug=developers&name=rob`}><a>Rob</a></Link>&nbsp;      
+      */}
 
       {children}
       <style>{`
